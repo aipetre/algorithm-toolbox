c631da39de0ed29cf21bd2f1eb939520aad496c2
Ionut Alexandru Petre <ionut_petre33@yahoo.com>
2017-07-31 23:11:26 +0300

    Add fibonnaci small & last digit implementations

---
+++ fibonnaci_small.py
@@ -0,0 +1,15 @@
+# Uses python3
+def fib_table(n):
+    if (n <= 1):
+        return n
+
+    table = [0 for i in range(0, n+1)]
+    table[0] = 0
+    table[1] = 1
+    for i in range(2, n+1):
+        table[i] = table[i-1] + table[i-2]
+
+    return table[n]
+
+n = int(input())
+print(fib_table(n))
