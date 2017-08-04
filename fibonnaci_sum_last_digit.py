7ee5e0ae52892b9a72c670c701154184ce560980
Ionut Alexandru Petre <ionut.petre@dpdgroup.co.uk>
2017-08-04 10:27:57 +0300

    Refactor `Last Digit of a Large Fibonacci Number`

--- fibonnaci_sum_last_digit.py
+++ fibonnaci_sum_last_digit.py
@@ -1,21 +1,42 @@
 # Uses python3
 import sys
 
-def fibonacci_sum_naive(n):
+def get_pisano_period(modulo):
+    a = 0
+    b = 1
+    c = a + b
+    for i in range(0, modulo * modulo):
+        c = (a + b) % modulo
+        a = b
+        b = c
+        if (a == 0 and b == 1):
+            return i + 1
+
+
+def get_fibonacci(n):
     if n <= 1:
         return n
 
     previous = 0
-    current  = 1
-    sum      = 1
+    current = 1
 
     for _ in range(n - 1):
-        previous, current = current, ((previous + current) % 10)
-        sum += current
+        previous, current = current, previous + current
+
+    return current
+
+
+def fibonacci_sum_fast(n, p):
+    sum = 0
+
+    for i in range(n+1):
+        fib_value = get_fibonacci(i % p)
+        sum += fib_value
 
     return sum % 10
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    print(fibonacci_sum_naive(n))
+    pisanoPeriod = get_pisano_period(10)
+    print(fibonacci_sum_fast(n, pisanoPeriod))
