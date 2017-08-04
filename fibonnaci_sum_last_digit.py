72dd2db2785619d9ed656ad7088fdeb303ccc6be
Ionut Alexandru Petre <ionut.petre@dpdgroup.co.uk>
2017-08-04 11:57:15 +0300

    Add `Sum of Fibonacci Numbers` implementation

--- fibonnaci_sum_last_digit.py
+++ fibonnaci_sum_last_digit.py
@@ -12,8 +12,7 @@ def get_pisano_period(modulo):
         if (a == 0 and b == 1):
             return i + 1
 
-
-def get_fibonacci(n):
+def get_fibonacci_last_digit(n):
     if n <= 1:
         return n
 
@@ -21,22 +20,28 @@ def get_fibonacci(n):
     current = 1
 
     for _ in range(n - 1):
-        previous, current = current, previous + current
+        previous, current = current, ((previous + current) % 10)
 
     return current
 
+""" The sum of the first n Fibonnaci numbers is
+actuall the F(n+2) - 1 value
+Calculates the Pisano period of 10
+Calculates the (n+2) % 10 Fibonnaci element 
+"""
+def fibonacci__last_digit_of_sum(n, p):
+    sum_last_digit = 0
 
-def fibonacci_sum_fast(n, p):
-    sum = 0
+    k = get_pisano_period(10)
+    element_to_find = (n+2) % k
 
-    for i in range(n+1):
-        fib_value = get_fibonacci(i % p)
-        sum += fib_value
-
-    return sum % 10
+    fib_value = get_fibonacci_last_digit(element_to_find)
+    sum_last_digit = fib_value - 1
+        
+    return sum_last_digit % 10
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
     pisanoPeriod = get_pisano_period(10)
-    print(fibonacci_sum_fast(n, pisanoPeriod))
+    print(fibonacci__last_digit_of_sum(n, pisanoPeriod))
